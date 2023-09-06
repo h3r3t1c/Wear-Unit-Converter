76cb1a79cd0c7aee80885d90b2d1af6453b5f58e
Thomas Otero <h3r3t1c@unm.edu>
2023-09-06 17:00:12 -0600

    Initial release for play store

--- app/src/main/java/com/h3r3t1c/wearunitconverter/ext/ScalingLazyListExt.kt
+++ app/src/main/java/com/h3r3t1c/wearunitconverter/ext/ScalingLazyListExt.kt
@@ -4,6 +4,6 @@ import android.util.Log
 import androidx.wear.compose.material.ScalingLazyListState
 
 val ScalingLazyListState.topAlpha: Float
-    get() = if(centerItemIndex <3){
+    get() = if(centerItemIndex <2){
         0.0f
     }else{ 1.0f}
